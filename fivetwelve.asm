0634f6b0355b4700ad3073cf49ead8014124d970
Jim Fowler <fowler@math.osu.edu>
2020-04-02 19:09:08 -0400

    playable version

--- fivetwelve.asm
+++ fivetwelve.asm
@@ -26,27 +26,27 @@
 base:	equ 0xfc80      ; Base of memory (in video!)
 board:	equ base	; 4x4 array for the current board
 seed:	equ base+16	; random seed
+changed:	equ base+18	; random seed	
 	
 graphics_mode:	
         mov ax,0xa000   ; Set DS = 0xA000, video memory
         mov ds,ax
         mov ax,0x0013   ; BIOS set VGA mode 13h
         int 0x10
-        cld		; FIXME: why is this needed?
 
 	mov word [seed], 17 	; initialize random seed
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 _init:
-	mov bx, board
 	mov di, 0
-	mov cx, 16
 clear_spot:
-	mov byte [bx+di], 0
-	inc di	
-	dec cx
+	mov byte [board+di], 0
+	inc di
+	cmp di, 16
 	jnz clear_spot
 
+	call add_if_any_empty
+	call add_if_any_empty	
 	jmp _draw
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -60,126 +60,110 @@ merge_blocks:
 	jne different_blocks
 	inc byte [bx]	    	; then merge the blocks...
 	mov byte [bx+di], 0	; but leave an empty space so additional blocks are not merged
+	mov byte [changed], 1	; and remember something changed
 different_blocks:		; else nothing to do 
 	ret
 
 merge_all_blocks:
 	push bx
+%rep 3
 	call merge_blocks
 	add bx, di
-	call merge_blocks
-	add bx, di
-	call merge_blocks
-	add bx, di	
+%endrep
 	pop bx
 	ret
 	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; merge blank space at bx and bx+di
-;;; destroys ax but preserves other registers	
 merge_blanks:
 	mov al, [bx+di]		; if [bx+di] is zero...
 	cmp al, 0
 	jne nonempty_blanks	
 	xchg al, [bx]		; then swap [bx] and [bx+di]
 	mov [bx+di], al
+	or byte [changed], al	; and remember something changed
 nonempty_blanks:		; else...
-	ret			; nothing to do 
+	ret			; do nothing
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; push tiles to one side
 merge_all_blanks:
-	push bx
-	call merge_blanks
-	add bx, di
-	call merge_blanks
-	add bx, di
-	call merge_blanks
-	add bx, di	
-	pop bx
-	ret	
-
-merge_and_move: 
-	call merge_all_blanks
-	call merge_all_blanks
-	call merge_all_blanks
-	call merge_all_blocks
-	call merge_all_blanks
-	call merge_all_blanks
-	call merge_all_blanks
+	push bx			; save the start of the board
+%rep 3				; for each of the 3 neighboring pairs of pairs
+	call merge_blanks	; push the tiles over
+	add bx, di		; move to next pair
+%endrep	
+	pop bx			; restore the pointer to the board
 	ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; get random number between 0 and 16
-random:
-	push bx
-	
-	mov ax, [seed]
-	mov bx, [seed]
-	
-	shl ax, 7
-	xor bx, ax
-
-	mov ax, bx
-	shr ax, 9
-	xor bx, ax
-
-	mov ax, bx
-	shl ax, 8
-	xor bx, ax
-
-	mov [seed], bx
-	mov ax, bx
-	and ax, 15
-	
-	pop bx
-
-	ret
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; clear keyboard buffer directly
-clearkey:
-	push ds
-	push es
-	mov ax,$40
-	mov es,ax
-	mov ds,ax
-	mov di,$1a
-	mov si,$1c
-	movsw
-	pop es
-	pop ds
+;;; push tiles, combine matches, and push again
+merge_and_move:
+%rep 3
+	call merge_all_blanks	; push tiles to the side
+%endrep
+	call merge_all_blocks	; combine two neighboring matching tiles
+%rep 3
+	call merge_all_blanks	; combining tiles may have result in gaps; push tiles again
+%endrep	
 	ret
 	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; add a random block at a blank space on the board
-add_random_block:
-try_again:	
-	call random
-	mov di, ax
-	mov ah, [board+di]
-	cmp ah, 0
-	jne try_again
-	mov byte [board+di], 1
-	ret
-
 add_if_any_empty:	
-	mov bx, board
+	mov bx, board		; bx points to the start of the board
+
 	mov di, 0
-	mov cx, 16
 check_spot:
-	mov al, [bx+di]
+	mov al, [bx+di]	
 	cmp al, 0
 	jz add_random_block
 	
 	inc di	
-	dec cx
+	cmp di, 16
 	jnz check_spot
 	ret
+
+add_random_block:
+try_again:
+	;;; get random number between 0 and 16
+	mov bx, [seed]		; load seed into bx
 	
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;	
+	mov ax, bx		; seed = seed xor (seed << 7)
+	shl ax, 7
+	xor bx, ax
+
+	mov ax, bx		; seed = seed xor (seed << 9)
+	shr ax, 9
+	xor bx, ax
+
+	mov ax, bx		; seed = seed xor (seed << 8)
+	shl ax, 8
+	xor bx, ax
+
+	mov [seed], bx		; store transformed seed 
+
+	mov di, bx		; di = random number between 0 and 15
+	and di, 15
+	
+	cmp byte [board+di], 0	; is that spot on the board empty?
+	jne try_again		; if nonempty, get another random spot
+	mov byte [board+di], 1	; put a "2" tile in that spot
+	
+	ret
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; transform the state of the game based on player input
 _update:
-	mov ah, 0
-	int 0x16
-	cmp ah, 0x48
+	mov ah, 0		; read a key (blocking) from bios
+	int 0x16		; the scan code is in ah
+	
+	cmp ah, 0x01		; escape key resets game
+	je _init
+
+	mov byte [changed], 0	; changed = 0, to record if anything changes
+	
+	cmp ah, 0x48		; jump to handlers for arrow keys
 	je keyup
 	cmp ah, 0x4b
 	je keyleft
@@ -187,95 +171,93 @@ _update:
 	je keyright
 	cmp ah, 0x50
 	je keydown
-	hlt
-	jmp _update
 
+	;; if we are here, we typed a key we ignore
+	hlt			; wait for something to happen
+	jmp _update		; continue the game loop
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;	
 keydown:
 	mov di, 4
-	
-	mov bx, board
+%assign i 0
+%rep 4	
+	mov bx, board+i
 	call merge_and_move
-
-	mov bx, board+1
-	call merge_and_move
-
-	mov bx, board+2
-	call merge_and_move
-
-	mov bx, board+3
-	call merge_and_move
-
+%assign i i+1
+%endrep	
 	jmp _draw
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;		
 keyleft:
 	mov di, -1
-	mov bx, board+3
-	call merge_and_move
 
-	mov bx, board+7
+%assign i 3
+%rep 4	
+	mov bx, board+i
 	call merge_and_move
-
-	mov bx, board+11
-	call merge_and_move
-
-	mov bx, board+15
-	call merge_and_move
-	
+%assign i i+4
+%endrep		
 	jmp _draw
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;		
 keyright:
 	mov di, 1
-	mov bx, board
+%assign i 0
+%rep 4	
+	mov bx, board+i
 	call merge_and_move
-
-	mov bx, board+4
-	call merge_and_move
-
-	mov bx, board+8
-	call merge_and_move
-
-	mov bx, board+12
-	call merge_and_move
-	
+%assign i i+4
+%endrep
 	jmp _draw
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;		
 keyup:
 	mov di, -4
-	mov bx, board+12
+%assign i 12
+%rep 4	
+	mov bx, board+i
 	call merge_and_move
-
-	mov bx, board+13
-	call merge_and_move
-
-	mov bx, board+14
-	call merge_and_move
-
-	mov bx, board+15
-	call merge_and_move
-
+%assign i i+1
+%endrep	
 	jmp _draw		
 	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;	
 _draw:
-	call add_if_any_empty
+	cmp byte [changed], 0
+	je nothing_changed
+	call add_if_any_empty	; if there's a blank space somewhere, add a new "2" tile
+nothing_changed:
 	
-	mov dx,4
+	mov dx,4		; for dx = 4 ... 0, the row
 draw_row:
-	mov cx,4
+	mov cx,4		; for cx = 4 ... 0, the column
 draw_col:
-	mov di, dx
+	mov di, dx		; di := 4*dx + cx
 	shl di, 2
 	add di, cx
-	mov al, byte [board+di-1-4]
+	mov al, byte [board+di-1-4] ; al := board[4*(dx-1) + (cx-1)]
 	
-	call drawbox	
+	call drawbox		; draw the tile with value al at (cx,dx)
 
-	dec cx
-	jnz draw_col
-	dec dx	
-	jnz draw_row
+	dec cx			; cx := cx - 1
+	jnz draw_col		; continue unless cx == 0
+	
+	dec dx			; dx := dx - 1
+	jnz draw_row		; continu unless dx == 0
+	
+	;; clear keyboard buffer directly
+	push ds			; save segment pointers
+	push es
+	mov ax,0x40		; es := 0x0040
+	mov es,ax
+	mov ds,ax		; ds := 0x0040
+	mov di,0x1a
+	mov si,0x1c
+	movsw			; set 0x40:0x1c = 0x40:0x1a
 
-	call clearkey
+	pop es			; restore segment pointers
+	pop ds
+	
 	hlt			; end of _draw
 	jmp _update	
 
@@ -330,7 +312,7 @@ drawbox:
 	inc cx
 	inc dx
 	add cx, 60
-	;call drawrect		
+	call drawrect		
 
 	pop dx
 	pop cx
@@ -350,7 +332,6 @@ drawbox:
 	mov cx, 1
 	or al, 48
 	int 0x10
-	
 	
 	pop dx
 	pop cx
